f471b69c81d463413aff9b93ce09ec1942c84e7a
malirezaie <m.alirezaie@gmail.com>
2018-06-05 22:39:38 -0400

    fix
    fix

--- Java/MobileAzureDevDays/app/appcenter-post-build.sh
+++ Java/MobileAzureDevDays/app/appcenter-post-build.sh
@@ -9,11 +9,11 @@ appcenter login --token $APPCENTER_TOKEN
 
 echo $APPCENTER_OUTPUT_DIRECTORY/*.apk
 
-APKFile= `find $APPCENTER_OUTPUT_DIRECTORY -name '*.apk'`
+#APKFile= `find $APPCENTER_OUTPUT_DIRECTORY -name '*.apk'`
 
 if ["$APPCENTER_SOURCE_DIRECTORY" == ""]; 
 then
-   appcenter test run espresso --app "Mahdis-Org/Java-Sample-AppCenter" --devices "Mahdis-Org/android-6-thru-8" --app-path $APKFile --test-series "master" --locale "en_US" --build-dir $CIRCLE_WORKING_DIRECTORY/app/build/outputs/apk 
+   appcenter test run espresso --app "Mahdis-Org/Java-Sample-CircleCI" --devices "Mahdis-Org/android-6-thru-8" --app-path $APKFile --test-series "master" --locale "en_US" --build-dir $CIRCLE_WORKING_DIRECTORY/app/build/outputs/apk 
 else
-    appcenter test run espresso --app "Mahdis-Org/Java-Sample-AppCenter" --devices "Mahdis-Org/android-6-thru-8" --app-path $APKFile --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Java/MobileAzureDevDays/app/build/outputs/apk
+    appcenter test run espresso --app "Mahdis-Org/Java-Sample-AppCenter" --devices "Mahdis-Org/android-6-thru-8" --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Java/MobileAzureDevDays/app/build/outputs/apk
 fi

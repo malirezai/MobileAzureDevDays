6a3a61fccfa281b9492535b95cc7d2a1f02b5bf4
malirezaie <m.alirezaie@gmail.com>
2018-06-05 22:35:12 -0400

    fix scrip
    fix script

--- Java/MobileAzureDevDays/app/appcenter-post-build.sh
+++ Java/MobileAzureDevDays/app/appcenter-post-build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-npm install npm@latest -g
+
 npm install -g appcenter-cli
 
 appcenter login --token $APPCENTER_TOKEN
 
+#build Android Tests
 ./../gradlew assembleDebugAndroidTest 
 
-APKFile= `find . ! -name '*Test.apk' -name '*.apk'`
+echo $APPCENTER_OUTPUT_DIRECTORY/*.apk
 
-echo $APKFile
+APKFile= `find $APPCENTER_OUTPUT_DIRECTORY -name '*.apk'`
+
 if ["$APPCENTER_SOURCE_DIRECTORY" == ""]; 
 then
    appcenter test run espresso --app "Mahdis-Org/Java-Sample-AppCenter" --devices "Mahdis-Org/android-6-thru-8" --app-path $APKFile --test-series "master" --locale "en_US" --build-dir $CIRCLE_WORKING_DIRECTORY/app/build/outputs/apk 

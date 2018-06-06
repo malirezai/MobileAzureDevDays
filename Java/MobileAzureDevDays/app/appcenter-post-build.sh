199e39708fca595670449b026cfd6ad2b924cd50
malirezaie <m.alirezaie@gmail.com>
2018-06-05 22:48:37 -0400

    fix circle yml
    fix circle yml

--- Java/MobileAzureDevDays/app/appcenter-post-build.sh
+++ Java/MobileAzureDevDays/app/appcenter-post-build.sh
@@ -7,13 +7,11 @@ appcenter login --token $APPCENTER_TOKEN
 #build Android Tests
 ./../gradlew assembleDebugAndroidTest 
 
-echo $APPCENTER_OUTPUT_DIRECTORY/*.apk
-
-#APKFile= `find $APPCENTER_OUTPUT_DIRECTORY -name '*.apk'`
-
 if ["$APPCENTER_SOURCE_DIRECTORY" == ""]; 
 then
-   appcenter test run espresso --app "Mahdis-Org/Java-Sample-CircleCI" --devices "Mahdis-Org/android-6-thru-8" --app-path $APKFile --test-series "master" --locale "en_US" --build-dir $CIRCLE_WORKING_DIRECTORY/app/build/outputs/apk 
+    echo $CIRCLE_ARTIFACTS/*.apk
+    appcenter test run espresso --app "Mahdis-Org/Java-Sample-CircleCI" --devices "Mahdis-Org/android-6-thru-8" --app-path $CIRCLE_ARTIFACTS/*.apk --test-series "master" --locale "en_US" --build-dir $CIRCLE_WORKING_DIRECTORY/app/build/outputs/apk 
 else
+    echo $APPCENTER_OUTPUT_DIRECTORY/*.apk
     appcenter test run espresso --app "Mahdis-Org/Java-Sample-AppCenter" --devices "Mahdis-Org/android-6-thru-8" --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Java/MobileAzureDevDays/app/build/outputs/apk
 fi

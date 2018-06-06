1bf7cc6126f40dfd290fa914ecb0ae8ec03b8af6
malirezaie <m.alirezaie@gmail.com>
2018-06-05 20:28:38 -0400

    fix script
    fix script

--- Java/MobileAzureDevDays/app/appcenter-post-build.sh
+++ Java/MobileAzureDevDays/app/appcenter-post-build.sh
@@ -5,5 +5,5 @@ npm install -g appcenter-cli
 appcenter login --token $APPCENTER_TOKEN
 
 APKFile=`find . -name '*Test.apk'`
-
-appcenter test run espresso --app "Mahdis-Org/Java-Sample-AppCenter" --devices "Mahdis-Org/latest-and-greatest" --app-path APKFile --test-series "master" --locale "en_US" --build-dir './app/build/outputs/apk'
+echo $APKFile
+appcenter test run espresso --app "Mahdis-Org/Java-Sample-AppCenter" --devices "Mahdis-Org/latest-and-greatest" --app-path APKFile --test-series "master" --locale "en_US" --build-dir '"$APPCENTER_SOURCE_DIRECTORY"'/app/build/outputs/apk'

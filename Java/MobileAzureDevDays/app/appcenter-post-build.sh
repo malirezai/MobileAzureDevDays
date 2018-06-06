9b85f121064664038f43293c4800340d829cd9dc
malirezaie <m.alirezaie@gmail.com>
2018-06-05 23:21:36 -0400

    fix script
    fix script

--- Java/MobileAzureDevDays/app/appcenter-post-build.sh
+++ Java/MobileAzureDevDays/app/appcenter-post-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+if ["$APPCENTER_BRANCH" == "test-branch"]; 
+then
 npm install -g appcenter-cli
 
 appcenter login --token $APPCENTER_TOKEN
-
 
 if ["$APPCENTER_SOURCE_DIRECTORY" == ""]; 
 then
@@ -17,4 +17,5 @@ else
 
     echo $APPCENTER_OUTPUT_DIRECTORY/*.apk
     appcenter test run espresso --app "Mahdis-Org/Java-Sample-AppCenter" --devices "Mahdis-Org/android-6-thru-8" --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Java/MobileAzureDevDays/app/build/outputs/apk
+fi
 fi
